b9c93a706c37691dbcb03f681897383712f72756
AkhilBarthwal005 <67703904+AkhilBarthwal005@users.noreply.github.com>
2021-02-19 20:51:18 +0530

    Add files via upload

---
+++ Lab-4/Client.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.io.*;
+import java.net.*;
+public class Client  {
+    //Client - Server two Way Communication using Socket Programming in Java{Single Machine}
+    public static void main(String[] args) throws Exception {
+        try {
+            Socket sk = new Socket("192.168.43.154",8080); // creating socket class object.
+            PrintWriter pw = new PrintWriter(sk.getOutputStream());
+            pw.println("Hello Server.. How are you???");
+            pw.flush(); // it will send all the character to the server.
+
+            // Receiving the Server Message.
+            InputStreamReader isr = new InputStreamReader(sk.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            String s = br.readLine();
+            System.out.println("Server: "+ s);
+            pw.close(); // connection has been closed here
+            sk.close();
+        }
+        catch (Exception e){
+            System.out.println("Error" + e);
+        }
+    }
+}
